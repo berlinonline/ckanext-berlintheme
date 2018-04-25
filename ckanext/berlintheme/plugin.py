bf9a5b8749e3da7e483c4b3fe87e90cc0bc999b6
Knud Möller <knud@datalysator.com>
2018-04-25 11:50:27 +0200

    add theme helpers from ckanext-berlin

--- ckanext/berlintheme/plugin.py
+++ ckanext/berlintheme/plugin.py
@@ -4,6 +4,7 @@ import os
 import logging
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckanext.berlintheme.helpers as theme_helpers
 from pylons import config
 
 log = logging.getLogger(__name__)
@@ -11,6 +12,7 @@ log = logging.getLogger(__name__)
 class BerlinTheme(plugins.SingletonPlugin):
 
     plugins.implements(plugins.IConfigurer, inherit=False)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # -------------------------------------------------------------------
     # Implementation IConfigurer
@@ -27,3 +29,20 @@ class BerlinTheme(plugins.SingletonPlugin):
 
         config['ckan.site_logo'] = "/images/berlin_open_data.png"
         config['ckan.favicon'] = "/favicon.ico"
+
+    # -------------------------------------------------------------------
+    # Implementation ITemplateHelpers
+    # -------------------------------------------------------------------
+
+    def get_helpers(self):
+        return {
+            'berlin_dataset_type_mapping': theme_helpers.dataset_type_mapping ,
+            'berlin_type_mapping_select_options': theme_helpers.type_mapping_select_options ,
+            'berlin_temporal_granularity_select_options': theme_helpers.temporal_granularity_select_options ,
+            'berlin_geo_granularity_select_options': theme_helpers.geo_granularity_select_options ,
+            'berlin_geo_coverage_select_options':
+                theme_helpers.geo_coverage_select_options ,
+            'berlin_state_mapping': theme_helpers.state_mapping ,
+            'berlin_user_orgs': theme_helpers.organizations_for_user ,
+            'berlin_is_sysadmin': theme_helpers.is_sysadmin ,
+        }
