db3fb9d0a5cf33c5d48b788e103ace6e8e339b73
Knud Möller <dunken69@gmail.com>
2022-03-30 11:39:19 +0200

    bring setup.py up-to-date

--- setup.py
+++ setup.py
@@ -1,31 +1,53 @@
 from setuptools import setup, find_packages
-import sys, os
+from os import path
 
-version = '0.1'
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
 	name='ckanext-berlintheme',
-	version=version,
-	description="Berlin Open Data Portal CKAN extension (Theme/UI only)",
-	long_description="""\
-	Contains the Theme/UI definitions for the Berlin Open Data Portal.
-	""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='Knud M\xc3\xb6ller',
-	author_email='knud.moeller@berlinonline.de',
+	version='0.3.0',
+	description="Theming/UI extension for the Datenregister (CKAN instance for Berlin's Open Data portal)",
+	long_description=long_description,
+
 	url='https://github.com/berlinonline/ckanext-berlintheme',
-	license='',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+
+  # author details
+	author='Knud Möller',
+	author_email='knud.moeller@berlinonline.de',
+	
+  license='AGPL',
+
+  # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+  classifiers=[
+    'Development Status :: 4 - Beta',
+
+    # Pick your license as you wish (should match "license" above)
+    'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3',
+  ],
+
+	keywords='''CKAN ITemplateHelpers IBlueprint theme ui templates''',
+
+	packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 	namespace_packages=['ckanext'],
+	
+  install_requires=[],
+
 	include_package_data=True,
 	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
 	entry_points=\
 	"""
-        [ckan.plugins]
-	         berlintheme=ckanext.berlintheme.plugin:BerlinTheme
+    [ckan.plugins]
+	  berlintheme=ckanext.berlintheme.plugin:BerlinTheme
+
+    [babel.extractors]
+    ckan = ckan.lib.extract:extract_ckan
 	""",
 )
